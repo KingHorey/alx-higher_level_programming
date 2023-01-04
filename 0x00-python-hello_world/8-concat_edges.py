30e9afd05c39b8bc503255267f80c143163ad4e2
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-04 01:28:35 +0100

    removed f-string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[38:65]} {str[-22: -18]}", end=" ")
-print(f"{str[0:7]}")
+print(str[38:66], str[-22: -18], end=" ")
+print(str[0:7])
