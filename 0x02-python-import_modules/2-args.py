b088a6d6d9c0b06461571f10536d6d9209989893
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-06 02:23:54 +0100

    fixed argv count print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ import sys
 def main():
 
     i = 1
-    if len(sys.argv) == 1:
-        print("{:d} argument".format(len(sys.argv) - 1))
-    else:
-        print("{:d} arguments".format(len(sys.argv) - 1))
+    if len(sys.argv) > 1:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
 
     for num in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(num, sys.argv[i]))
