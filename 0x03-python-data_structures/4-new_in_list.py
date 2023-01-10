35c3dfcb094c409d0b571e3522851fc801463326
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-10 02:03:56 +0100

    replacing index in a copied list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     rng = len(my_list)
     a = my_list.copy()
-    for i in my_list:
+    for i in range(0, rng):
         if idx < 0 or idx > (rng - 1):
             return a
         elif i == idx:
