d31f2e1d5d0e2eaef42b0a3e011016201ab999e0
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-10 20:16:19 +0100

    fixed pycodestyle and if list is None

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,12 +4,9 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > lgth or my_list is None:
         return (my_list)
     else:
-        new_list = my_list
         i = 0
         for i in range(0, lgth):
-            if i != idx:
-                new_list[i] = my_list[i]
-            else:
+            if i == idx:
                 del my_list[i]
 
-    return (new_list)
+    return (my_list)
