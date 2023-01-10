6aa4947b02d001d9f078f749c81c92bda642a783
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-10 20:10:03 +0100

    delete at position

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx = 0):
+def delete_at(my_list=[], idx=0):
     lgth = len(my_list)
     if idx < 0 or idx > lgth or my_list is None:
         return (my_list)
     else:
         new_list = my_list
         i = 0
-        for i in range(0, lgth - 1):
+        for i in range(0, lgth):
             if i != idx:
                 new_list[i] = my_list[i]
             else:
