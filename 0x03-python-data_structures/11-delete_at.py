558374370f6fabea6d19453b39648034a579cc61
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-10 20:07:12 +0100

    delete ar position

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx = 0):
+    lgth = len(my_list)
+    if idx < 0 or idx > lgth or my_list is None:
+        return (my_list)
+    else:
+        new_list = my_list
+        i = 0
+        for i in range(0, lgth - 1):
+            if i != idx:
+                new_list[i] = my_list[i]
+            else:
+                del my_list[i]
+
+    return (new_list)
