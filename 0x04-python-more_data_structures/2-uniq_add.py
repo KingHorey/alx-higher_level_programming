831baa7ec327558376471118a019cf67557ce71c
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-12 00:48:01 +0100

    unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+def uniq_add(my_list=[]):
+    if not my_list:
+        return my_list
+    else:
+        result = list(set(my_list))
+        x = 0
+        new_list = sum(result)
+        
+    return(new_list)
