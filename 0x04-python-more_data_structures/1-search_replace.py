033a6d17a7de8bc83b0967211a3f4e1cd6ea7e7d
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-11 21:13:50 +0100

    changed return value

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if not my_list:
-        return
+        return my_list
     else:
         new_list = my_list.copy()
         result = map(lambda x: replace if x == search else x, new_list)
