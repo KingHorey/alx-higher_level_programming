fc563e39282b18852783b4de53fa320748e422ab
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-12 01:21:41 +0100

    changed return if there are no keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     if not a_dictionary:
-        return None
+        return 0
     else:
         count = 0
         for i in a_dictionary.keys():
