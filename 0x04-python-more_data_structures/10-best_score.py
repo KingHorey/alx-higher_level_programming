e0eeab9cba16eb40e60b875d411faccaa65eb23b
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-12 03:21:45 +0100

    if no max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    elif len(a_dictionary) == 0:
+    elif len(a_dictionary.keys()) == 0:
         return None
     else:
         hghst = max(a_dictionary)
