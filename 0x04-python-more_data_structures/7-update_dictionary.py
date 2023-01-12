34c699ed4ae7d7bc5ac3778ce5a70ee02b192d02
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-12 02:04:25 +0100

    added conditional if key, value is None

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,10 @@
 def update_dictionary(a_dictionary, key, value):
     if not a_dictionary:
         return 0
+    if not key:
+        return a_dictionary
+    if not value:
+        return a_dictionary
     else:
         a_dictionary.update({key: value})
 
