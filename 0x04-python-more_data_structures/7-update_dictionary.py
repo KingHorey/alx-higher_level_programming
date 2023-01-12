236da7b6a42ae7507cbf44c69b1120577897aa7a
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-12 03:53:20 +0100

    update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not a_dictionary:
-        return None
-    if not key:
-        return a_dictionary
-    if not value:
-        return a_dictionary
-    else:
-        a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
 
     return (a_dictionary)
