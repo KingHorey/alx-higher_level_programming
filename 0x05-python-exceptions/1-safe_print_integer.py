4dc8f6f575875b2a6b4862d8c80e807ccd8e988d
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-24 00:58:25 +0100

    print int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+        try:
+            name = int(value)
+            print("{:d}".format(name))
+            return (True)
+        except ValueError:
+            return False
