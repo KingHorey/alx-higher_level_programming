011997f16823c7fd8222bf5260375e1ac1cad858
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-24 00:44:43 +0100

    print element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        lgth = sum(1 for x in my_list)
-        i = 0
-        if x >= lgth:
-            x = lgth
-        while i < x:
-            if (i == x - 1):
-                print("{:s}".format(str(my_list[i])))
-            else:
-                print("{:s}".format(str(my_list[i])), end="")
-            i += 1
-    except (len(my_list) == 0):
-        print("Empty list")
+    if (x > 0):
+        try:
+            lgth = sum(1 for x in my_list)
+            i = 0
+            if x >= lgth:
+                x = lgth
+            while i < x:
+                print(my_list[i], end="")
+                i += 1
+        except IndexError:
+            print("Out of bounds")
+    print()
     return (i)
