b146e510d5be1e028dd5c9d16d5171aa9aa32a44
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-24 01:46:59 +0100

    text conditional

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,9 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end="")
                 counter += 1
-            except (TypeError, ValueError):
+            except TypeError:
+                continue
+            except ValueError:
                 continue
         print()
         return (counter)
