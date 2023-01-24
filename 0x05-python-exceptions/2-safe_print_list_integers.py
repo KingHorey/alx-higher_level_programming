38369bb516e79a24aa463d6ee5567d6f1a045922
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-24 01:32:22 +0100

    test conditional

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     lgth = sum(1 for i in my_list)
-    if (x <= lgth):
+    if (x <= lgth or x < 0):
         i = 0
         counter = 0
         for i in range(x):
