0f4b23f694b849c59bb71e35f13618818f8d0fe6
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-25 14:02:51 +0100

    coordinates

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""
+No Modules imported
+"""
+
+
+class Square:
+    """Class gets the coordinates of a square and
+    makes uses of getters and setters"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if self.__position[0] < 0 or self.__position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            i = 0
+            while i < self.__size:
+                for j in range(self.position[0]):
+                    print(" ", end="")
+                for x in range(self.__size):
+                    print("#", end="")
+                print()
+                i += 1
