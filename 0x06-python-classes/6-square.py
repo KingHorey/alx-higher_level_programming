676e19b67c64e5731b01ef56d5a672b0c3a54b23
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-27 07:52:41 +0100

    changed loop

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,6 +47,6 @@ class Square:
             print()
         else:
             for j in range(self.__position[1]):
-                print("", end="")
+                print("")
             for x in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
