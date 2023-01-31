c46ceef9933f9829e7775a1fd2719c1d0e8859fe
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-01-31 14:51:08 +0100

    list copy

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-"""No modules imported"""
-
-
 def copy_list(l):
-    """This function returns the copy of a list"""
-
-    new_list = l[:]
-
-    return new_list
+    return (l.copy())
