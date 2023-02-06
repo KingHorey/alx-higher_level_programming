ae9b9c83652483de3e4fdc83ad890bf0119cb561
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-06 17:33:15 +0100

    Raise EXCEPTION

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""NO modules imported"""
+
+
+class BaseGeometry:
+    """
+    Geometry class for further operations
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
