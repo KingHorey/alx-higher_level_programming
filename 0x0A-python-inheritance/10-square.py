bdc6bdc6005971c3986d4b926e7680f7e71efc23
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-07 02:21:18 +0100

    add docstring

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ """
+    """ Class defines the dimension for a square object using
+    methods inherited from Rectangle
+    """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
@@ -16,6 +19,7 @@ class Square(Rectangle):
         return (self.__size ** 2)
 
     def __str__(self):
+        """Returns the dimension of the square"""
         result = "[" + self.__class__.__name__ + "] "
         result += str(self.__size) + "/" + str(self.__size)
         return result
