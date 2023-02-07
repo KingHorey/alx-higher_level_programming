c2e3a6646ef43b06173887a769ffed3520c25090
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-07 20:46:32 +0100

    selective dict representation

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" NO modules imported """
+
+
+class Student:
+    """
+    Defines a student class and can initialise the
+    details of the student into an object that can
+    be saved as a JSON file
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialise instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns a dictionary representation of
+        the instance"""
+        if attrs:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+        return (self.__dict__)
