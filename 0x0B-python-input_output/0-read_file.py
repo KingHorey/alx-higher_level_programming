0e6199cddf9e6bd00ccc707210d096a7046254d3
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-07 12:25:21 +0100

    fixed encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" No modules imported """
+""" Imports sys module """
 
 
 def read_file(filename=""):
     """ This function reads information in a txt file and prints to stdout"""
 
-    with open(filename,'r', encoding="UTF-8") as f:
-        print(f.read())
+    with open(filename, encoding="UTF8") as f:
+        print(f.read(), end='')
