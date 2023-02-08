f2f3352aa2216d430e73af4d1af605e0d7ce00b2
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-08 04:07:13 +0100

    magic print

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/pyhton3
-
-""" NO modules imported"""
-
-
 def magic_string():
-    """This function returns a string n times the number
-    of iteration"""
-    magic_srings = getattr(magic_string, "n", 0) + 1
-    return ', '.join(["BestSchool" for i in range(magic_strings)])
+    magic_srings.n = getattr(magic_string, "n", 0) + 1
+    return ', '.join(["BestSchool" for i in range(magic_strings.n)])
