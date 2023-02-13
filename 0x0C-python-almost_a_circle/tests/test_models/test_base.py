f14b9e0de1ccb0f2c1c619fba4b91631a32f6e36
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-13 16:44:59 +0100

    new cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -42,3 +42,21 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b1.id = True
         self.assertEqual(True, b1.id)
+
+    def test_to_jsonstr(self):
+        info1 = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        info2 = {'x': 4, 'width': 11, 'id': 2, 'height': 4, 'y': 2}
+        self.assertEqual(str, type(Base.to_json_string([info1])))
+        self.assertEqual(str, type(Base.to_json_string([info1])))
+        self.assertEqual(str, type(Base.to_json_string([info1, info2])))
+
+    def test_to_json(self):
+        info = Rectangle(1, 3, 4, 5)
+        info2 = Rectangle(4, 5, 12, 13)
+        info = info.to_dictionary()
+        info2 = info2.to_dictionary()
+        self.assertEqual(str, type(Base.to_json_string([info])))
+        self.assertEqual(str, type(Base.to_json_string([info, info2])))
+        self.assertEqual("[]", Base.to_json_string(None))
+
+
