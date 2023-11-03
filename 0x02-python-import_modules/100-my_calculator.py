cf871c6280a549514edbeb9ad9072dbd00a65a1e
kinghorey <oreoluwaakinbo.oa@gmail.com>
2023-11-03 08:53:13 +0100

    remove operator to elif

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,16 +7,16 @@ from calculator_1 import add, mul, div, sub
 def calculate():
     """ handles basic arithmetic """
     keys = ["*", "/", "+", "-"]
-    operator = sys.argv[2]
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif operator not in keys:
+    elif sys.argv[2] not in keys:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     else:
         arg_1 = sys.argv[1]
         arg_2 = sys.argv[3]
+        operator = sys.argv[2]
         if sys.argv[2] == "*":
             result = mul(int(arg_1), int(arg_2))
             print("{} {} {} = {}".format(arg_1, operator, arg_2, result))
