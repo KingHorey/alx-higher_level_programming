42493f168d3db1205ccbd6ae6485399dcfedcc1d
kinghorey <oreoluwaakinbo.oa@gmail.com>
2023-11-21 02:25:58 +0100

    return number of line on IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
             return(i)
         except IndexError:
             print()
-            return (0)
+            return (i)
     else:
         y = -(lgth)
         count = 0
