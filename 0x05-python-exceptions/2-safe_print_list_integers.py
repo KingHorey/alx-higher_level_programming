c702b03bc8afda1147b4040603fc6f37513d4eeb
kinghorey <oreoluwaakinbo.oa@gmail.com>
2023-11-21 03:25:22 +0100

    remove isinstance and added except checks for TypeError and ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,9 +8,13 @@ def safe_print_list_integers(my_list=[], x=0):
         lgth = sum(1 for i in my_list)
         count = 0
         for i in range(x):
-            if isinstance(my_list[i], int):
+            try:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
+            except ValueError:
+                continue
+            except TypeError:
+                continue
         print()
         return (count)
     else:
