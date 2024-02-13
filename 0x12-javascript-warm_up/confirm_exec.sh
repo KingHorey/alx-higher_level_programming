3b2a7700b3adccfc49f6ba5e8f9fd4c1abbf3978
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-13 03:13:50 +0100

    Add executable bit to all files

--- 0x12-javascript-warm_up/confirm_exec.sh
+++ 0x12-javascript-warm_up/confirm_exec.sh
@@ -2,7 +2,6 @@
 
 # This script confirms that the executable bit is set on a file.
 
-# path="~/repositories/alx-higher_level_programming/
 for file in *;
 do
 	if [ -x "$file" ]
@@ -15,7 +14,12 @@ do
 	fi
 done
 
-git add .
-git commit -m "Add executable bit to all files"
-git push
+# git add all files
+read -p "Do you want to add all files to the repository? (y/n) " response
+if [ "$response" = "y" ]
+then
+	git add .
+	git commit -m "Add executable bit to all files"
+	git push
+fi
 
