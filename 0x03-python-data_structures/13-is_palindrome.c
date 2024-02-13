0af88af42b45d62bfc8e44b4f49b666e3889083e
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-13 02:39:57 +0100

    process argv

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * is_palindrome - checks if a linked list is a
@@ -10,42 +11,44 @@
 
 int is_palindrome(listint_t **head)
 {
+	// printf("Mem addr: %p/n", &(**head));
 	if (*head == NULL)
 		return (0);
 	else if ((*head)->next == NULL)
 		return (1);
-	listint_t *ptr, *ptr_data;
+	listint_t *ptr, **ptr_data;
+	int ptr_check;
 
 	ptr = *head;
-	ptr_data = *head;
-	ptr = recurse(ptr);
-	while (ptr_data && ptr)
-	{
-		if (ptr_data->n == ptr->n)
-		{
-			if (ptr_data->next == NULL)
-				return (1);
-		}
-		if (ptr_data->n != ptr->n)
-			return (0);
-		ptr_data = ptr_data->next;
-	}
+	ptr_data = head;
+	ptr_check = recurse(ptr_data, ptr);
+	if (ptr_check)
+		return (ptr_check);
 	return (0);
 }
 
 /**
  * recurse - recurse the linked list till end
- *
+ * @ptr_head - a pointer to the head of the linked list
  * @ptr: pointer to the head of the linked list
  *
  * Return: NULL if pointer is NULL or ptr at end of noode
  */
 
-listint_t *recurse(listint_t *ptr)
+int recurse(listint_t **ptr_head, listint_t *ptr)
 {
-	if (ptr->next == NULL)
-		return (ptr);
-	ptr = ptr->next;
-	return (recurse(ptr));
+	int result_checker, final_result;
+
+	if (ptr == NULL)
+		return (1);
+	result_checker = recurse(ptr_head, ptr->next);
+	*ptr_head = (*ptr_head)->next;
+	if (result_checker == 0)
+		return 0;
+	final_result = (*ptr_head)->n == ptr->n;
+	*ptr_head = (*ptr_head)->next;
+
+	return (final_result);
+
 }
 
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,8 +23,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-listint_t recourse(listint_t *ptr);
 int is_palindrome(listint_t **head);
-listint_t *recurse(listint_t *ptr);
+int recurse(listint_t **, listint_t *ptr);
 
 #endif
