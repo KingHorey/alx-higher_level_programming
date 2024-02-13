4734d9db5d53f338b67ed14318e886c1676f177b
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-13 04:01:13 +0100

    change logic conditional of else if

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-if (num && num >= 0) {
+if (num) {
   let i = 0;
   while (i < num) {
     console.log('C is fun');
     i++;
   }
-} else if (!num){
+} else if (!(num <= 0)){
   console.log('Missing number of occurrences');
 }
