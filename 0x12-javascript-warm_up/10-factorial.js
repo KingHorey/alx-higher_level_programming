b8493aa321182eaa39c1307ce56856e43cc746b0
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-13 04:19:44 +0100

    Add executable bit to all files

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-
 function factorial (num) {
-  if (num === 0) {
-    return 1;
+  if (isNaN(num)) {
+    return (1);
+  } else if (num === 0) {
+    return (1);
   } else {
-    return num * factorial(num - 1);
+    return (num * factorial(num - 1));
   }
 }
 console.log(factorial(num));
