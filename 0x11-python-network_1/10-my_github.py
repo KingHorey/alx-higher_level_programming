a612dffac8e5a6bf66610203f5a8466080dd179a
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 04:06:51 +0100

    pycodestyle validation

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,10 +8,10 @@ from sys import argv
 def getGithubAPI():
     """ get github api """
     url = 'https://api.github.com/user'
-    username = argv[1]
+    user = argv[1]
     password = argv[2]
     headers = {'Authorization': 'token {}'.format(password)}
-    response = requests.get(url, headers=headers, params={'username': username})
+    response = requests.get(url, headers=headers, params={'username': user})
     print(response.json().get('id'))
 
 
