cdb453e6890d307dc829aa0b38b7cbc7dd365115
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 02:10:09 +0100

    import requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 """ import requests """
-import requests as req
+import requests
 
 
 def runRequests():
     """ get the response from the server """
-    response = req.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
