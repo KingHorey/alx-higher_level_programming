b366c50c4ce4b87b8c740325b30d8d04d275a4cf
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 02:52:34 +0100

    send mail to test json response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" import requests and sys """
+import requests
+import sys
+
+
+def send_mail_get_json():
+    """ sends a POST request to server """
+
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': q}
+
+    response = requests.post(url, data)
+    try:
+        response = response.json()
+        if response:
+            print("{[]} {}".format(response.get('id'), response.get('name')))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    send_mail_get_json()
