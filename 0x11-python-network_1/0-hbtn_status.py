24fb2e99863e09baa71102bddd43a88c9ecac5d7
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 01:29:40 +0100

    add __name__ == __main__

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,13 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("- type: {}$".format(type(content)))
-    print("- content: {}$".format(content))
-    print("- utf8 content: {}$".format(content.decode('utf-8')))
+def getContent():
+    """ Get the content of the url """
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("- type: {}$".format(type(content)))
+        print("- content: {}$".format(content))
+        print("- utf8 content: {}$".format(content.decode('utf-8')))
+
+if __name__ == "__main__":
+    getContent()
