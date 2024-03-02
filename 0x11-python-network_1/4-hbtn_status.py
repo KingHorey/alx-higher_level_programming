8987fd9124cd213fbf739406fadcfd1bd1a38a8b
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 02:19:54 +0100

    remove function

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,14 +4,9 @@
 import requests
 
 
-def runresponse():
-    """ get the response from the server """
+if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
-
-
-if __name__ == "__main__":
-    runresponse()
