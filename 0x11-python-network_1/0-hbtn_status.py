379866ee79e1fec856f5d12b2ac02772448c7611
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 01:34:15 +0100

    pycodestyle validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,16 +3,19 @@
 """ import urllib """
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-req = urllib.request.Request(url)
+
 def getContent():
     """ Get the content of the url """
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+
     with urllib.request.urlopen(req) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
 
 if __name__ == "__main__":
     getContent()
