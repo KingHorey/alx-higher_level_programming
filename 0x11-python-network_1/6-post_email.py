fd9d483f5e7161cab81035a7d09f9382e1f050b7
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-02 02:27:07 +0100

    send post mail using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" import requests and sys modules """
+import requests
+import sys
+
+
+def sendMail():
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data)
+
+    print("Your email is: {}".format(response.text))
+
+
+if __name__ == "__main__":
+    sendMail()
