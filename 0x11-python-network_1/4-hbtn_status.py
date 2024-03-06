a98a81eecc6e4bbf3749e5669254b59ef5e16199
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-06 04:28:22 +0100

    pycodestyle validation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 """ import requests """
 import requests
 
+
 def status():
     """ function that fetches https://alx-intranet.hbtn.io/status"""
     url = "https://alx-intranet.hbtn.io/status"
@@ -10,6 +11,7 @@ def status():
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
+
 
 if __name__ == "__main__":
     status()
