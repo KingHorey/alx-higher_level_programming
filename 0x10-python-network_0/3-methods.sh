88486987c5d6fa485870d06dc73090b23689d40e
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-17 23:18:57 +0100

    conform to alx deprecared requirement

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ASK SERVER TO RETURN A LIST OF METHODS IT SUPPORTS
-curl -sI --request OPTIONS "$1" | grep -i "Allow" | cut -d " " -f 2
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
