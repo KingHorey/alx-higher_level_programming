1e8f2793d77038b302bcae3b57551f576ce255b8
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-27 04:08:13 +0100

    remove extra whitespace betwwen statusCode and 'code' text

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request(process.argv[2], (err, res, body) => {
-  if (err) { console.error(err); } else { console.log('code: ', res.statusCode); }
+  if (err) { console.error(err); } else { console.log('code:', res.statusCode); }
 });
